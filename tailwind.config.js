9690e59709e2928872789ca24fe387786bbae080
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-03-02 03:04:18 +0700

    add custom font, color and box shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bodyColor: "#030D25",
+      },
+      fontFamily: {
+        bodyFont: ["Poppins", "sans-serif"],
+        titleFont: ["Montserrat", "sans-serif"],
+      },
+      boxShadow: {
+        todoShadow: "0px 0px 20px 4px rgba(11,4,209,1)",
+      },
+    },
   },
   plugins: [],
 };
