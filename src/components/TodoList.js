62858193e1992425e6d3f8f43ea16194a53be46d
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-03-03 20:54:10 +0700

    add todo value and animate if add new todo

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,11 +1,15 @@
 import React, { useState } from "react";
 import { MdDelete } from "react-icons/md";
+import { motion } from "framer-motion";
 
-const TodoList = ({ todoValue }) => {
+const TodoList = ({ todo }) => {
   const [mark, setMark] = useState(false);
 
   return (
-    <li
+    <motion.li
+      initial={{ y: 10, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{ y: { type: "spring", stiffness: 120 } }}
       onClick={() => setMark(!mark)}
       className={`${
         mark
@@ -13,11 +17,11 @@ const TodoList = ({ todoValue }) => {
           : "border-l-green-500 border-green-900"
       } w-full font-titleFont font-medium text-base border-[1px] border-l-[6px] p-2 cursor-pointer  flex items-center justify-between`}
     >
-      {todoValue}
+      {todo}
       <span className="text-xl text-gray-300 hover:text-red-500 duration-300 cursor-pointer">
         <MdDelete />
       </span>
-    </li>
+    </motion.li>
   );
 };
 
