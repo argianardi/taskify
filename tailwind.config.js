00b366fbab602f5472aaf2e6e913b36d53cd6b89
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-03-05 01:25:24 +0700

    add custom screen size

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,18 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      screens: {
+        xs: "320px",
+        sm: "375px",
+        sml: "500px",
+        md: "667px",
+        mdl: "768px",
+        lg: "960px",
+        lgl: "1024px",
+        xl: "1280px",
+      },
       colors: {
-        bodyColor: "#030D25",
+        bodyColor: "#1d1e2c",
       },
       fontFamily: {
         bodyFont: ["Poppins", "sans-serif"],
